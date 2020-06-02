5e234ece75456ea430596b7d9cc4ef390525430f
Serg <sbezuglii@mail.ru>
2020-06-02 14:30:46 +0300

    2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.

--- homeWork_1.playground/Contents.swift
+++ homeWork_1.playground/Contents.swift
@@ -26,3 +26,26 @@ if discr > 0 {
     print("Корней нет")
 }
 
+
+
+print("\r\n")
+print("2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.\r\n")
+
+let k1: Double = 23.4
+let k2: Double = 65.8
+
+
+var gPow = pow(k1, 2) + pow(k2, 2)
+var g = sqrt(gPow)
+
+var perimeter = k1 + k2 + g
+
+// узнаем площадь формулой Герона, где p - полупериметр
+
+var p = perimeter / 2
+var area = sqrt(p * (p - k1) * (p - k2) * (p - g))
+
+
+print("Катет1: \(k1), катет2: \(k2)")
+print("Гипотенуза: \(g), периметр: \(perimeter), площадь: \(area)")
+
