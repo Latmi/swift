ccd9ee3eb7d0566e30c4cd253fbe2ad11274b4e0
Serg <sbezuglii@mail.ru>
2020-06-02 14:30:09 +0300

    1. Решить квадратное уравнение.

--- homeWork_1.playground/Contents.swift
+++ homeWork_1.playground/Contents.swift
@@ -1,1 +1,28 @@
+import Foundation
+
+print("1. Решить квадратное уравнение.\r\n")
+print("Уравнение: 2x^2 + 4x + 2 = 0")
+
+var a: Double = 3.2
+var b: Double = -7.8
+var c: Double = 1
+
+var y: Double = 4
+
+var discr = pow(Double(b), 2) - y * a * c
+
+
+if discr > 0 {
+    
+    let x1 = (-b + sqrt(discr)) / (2 * a)
+    let x2 = (-b - sqrt(discr)) / (2 * a)
+    print("Дискриминант: \(discr), первый корень: \(x1), второй корень: \(x2)")
+    
+} else if discr == 0 {
+    let x1 = -b / (2 * a)
+    print("Дискриминант: \(discr), корень: \(x1)")
+    
+} else {
+    print("Корней нет")
+}
 
